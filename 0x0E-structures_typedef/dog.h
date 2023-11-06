0483fbbe412d7f0a4a5bbe13a16198b14dc247a4
ThutoMotaung <136109535+ThutoMotaung@users.noreply>
2023-11-06 13:48:12 -0800

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,10 +12,16 @@
 
 struct dog
 {
-                char *name;
-		float age;
-		char *owner;
+char *name;
+float age;
+char *owner;
 };
+
+
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -27,3 +33,4 @@ int _strlen(char *s);
 
 
 #endif
+
