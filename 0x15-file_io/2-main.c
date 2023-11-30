fda63e0aedb26ba23c1b58f57109f12adadc88c7
ThutoMotaung <136109535+ThutoMotaung@users.noreply>
2023-11-30 11:52:31 -0800

    0x15. C - File I/O

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -7,12 +7,11 @@
 *
 * Return: Always 0.
 */
-int main(int ac, char **av)
+int main(int ac,  char **av)
 {
 int res;
-
 if (ac != 3)
-}
+{
 dprintf(2, "Usage: %s filename text\n", av[0]);
 exit(1);
 }
